53b6695ddf484d49684a12862cc63057df86068f
Emily Guo <yuchen0721@gmail.com>
2018-01-27 03:48:05 +0000

    style errors and msg particle

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,8 +22,9 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
+    
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -33,7 +34,7 @@ class ArticlesController < ApplicationController
   def update
    
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -48,7 +49,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
@@ -60,4 +61,8 @@ class ArticlesController < ApplicationController
     def article_params
     params.require(:article).permit(:title, :description)
     end
+    
+    def size
+    end
+    
 end
