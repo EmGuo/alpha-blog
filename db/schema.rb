17404f130b7f21702043f983430396563928f0c0
Emily Guo <yuchen0721@gmail.com>
2018-09-19 03:02:25 +0000

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223022204) do
+ActiveRecord::Schema.define(version: 20180901232625) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20180223022204) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
